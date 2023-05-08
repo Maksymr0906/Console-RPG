9d3b3157a3594e91a5dc63912349ba1e88904a62
Maksymr0906 <maksymr0906@gmail.com>
2023-05-08 16:04:59 +0300

    make a player constructor accessors and modifiers

--- header/Player.hpp
+++ header/Player.hpp
@@ -12,26 +12,52 @@ class Player {
 private:
     static constexpr const int def_health = 100;
     static constexpr const int def_max_health = 100;
-    static constexpr const int def_damage = 20;
+    static constexpr const int def_min_damage = 5;
+    static constexpr const int def_max_damage = 10;
+    static constexpr const int def_xp = 0;
+    static constexpr const int def_level = 1;
+    static constexpr const char *def_name = "Unnamed";
 protected:
-    int health, maxHealth, damage;
+    std::string name;
+    int health, maxHealth, minDamage, maxDamage, xp, level;
     std::vector<Item> inventory;
 public:
-    Player(int health = def_health, int maxHealth = def_max_health, int damage = def_damage, std::vector<Item> inventory = std::vector<Item>(0))
-    :health{health}, maxHealth{maxHealth}, damage{damage}, inventory{inventory} {};
+    //Constructors
+    Player();
 
+    //Accessors
     int get_health() {return health;}
-    void set_health(int health) {this->health = health;}
     int get_maxHealth() {return maxHealth;}
-    void set_maxHealth(int maxHealth) {this->maxHealth = maxHealth;}
-    int get_damage() {return damage;}
-    void set_damage(int damage) {this->damage = damage;}
+    int getMinDamage() {return minDamage;}
+    int getMaxDamage() {return maxDamage;}
+    int getXp() {return xp;}
+    int getLevel() {return level;}
+    std::string getName() {return name;}
 
-    void takeDamage(int damage) {health -= damage;}
+    //Modifiers
+    void setHealth(int health) {this->health = health;}
+    void setMaxHealth(int maxHealth) {this->maxHealth = maxHealth;}
+    void setMinDamage(int minDamage) {this->minDamage = minDamage;}
+    void setMaxDamage(int maxDamage) {this->maxDamage = maxDamage;}
+    void setXp(int xp) {this->xp = xp;}
+    void setLevel(int level) {this->level = level;}
+    void setName(std::string name) {this->name = name;}
+
 
     void showInventory();
     bool isAlive();
 };
+
+Player::Player() {
+    name = def_name;
+    health = def_health;
+    maxHealth = def_health;
+    minDamage = def_min_damage;
+    maxDamage = def_max_damage;
+    xp = def_xp;
+    level = def_level;
+    inventory = std::vector<Item>(0);
+}
 
 void Player::showInventory() {
     if(inventory.empty()) {
--- source/main.cpp
+++ source/main.cpp
@@ -14,5 +14,4 @@ int main() {
     while(game.getPlaying()) {
         game.mainMenu();
     }
-
 }
