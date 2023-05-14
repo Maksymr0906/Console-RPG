85cd9a5e35f805fb7e8fa4193cf71fa1b89b7bc7
Maksymr0906 <maksymr0906@gmail.com>
2023-05-14 20:33:01 +0300

    upd showInventory

--- source/Inventory.cpp
+++ source/Inventory.cpp
@@ -12,12 +12,25 @@ void Inventory::addItem(std::unique_ptr<Item> &item) {
 
 void Inventory::showInventory() {
     if(this->inventory.empty()) {
-        std::cout << "Inventory is empty" << std::endl;
+        std::cout << "Your inventory is empty" << std::endl;
         return ;
     }
-    std::cout << "INVENTORY" << std::endl;
-    for(const auto &item: this->inventory) {
-        std::cout << *item << std::endl;
+    std::cout << "Your inventory" << std::endl;
+    for(size_t i = 0; i < this->inventory.size(); i++) {
+        std::cout << "(" << i+1 << ") - " << this->inventory.at(i)->getName() << std::endl; 
+    }
+    
+    std::cout << "Select the corresponding number to read more about this item or " << this->inventory.size()+1 << " to go back: ";
+
+    int choice{};
+    std::cin >> choice;
+
+    if(choice == this->inventory.size()+1) {
+        return;
+    }
+    else {
+        std::cout << *this->inventory.at(choice-1) << std::endl;
+        this->showInventory();
     }
 }
 
